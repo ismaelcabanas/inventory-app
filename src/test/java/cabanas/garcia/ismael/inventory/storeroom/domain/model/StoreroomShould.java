10e7ba7ba9bcfb85e4772c255f073bdf7be8fce7
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-02-12 23:07:46 +0100

    Unit test for get stock of product

--- src/test/java/cabanas/garcia/ismael/inventory/storeroom/domain/model/StoreroomShould.java
+++ src/test/java/cabanas/garcia/ismael/inventory/storeroom/domain/model/StoreroomShould.java
@@ -1,5 +1,6 @@
 package cabanas.garcia.ismael.inventory.storeroom.domain.model;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,11 +16,17 @@ public class StoreroomShould {
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    @Test public void
-    fill_with_new_amount_of_given_product() {
-        Storeroom storeroom = new Storeroom(new StoreroomId(), STOREROOM_NAME);
+    private Storeroom storeroom;
+
+    @Before public void
+    setUp() {
+        storeroom = new Storeroom(new StoreroomId(), STOREROOM_NAME);
         ProductStock productStock = new ProductStock(SOME_PRODUCT_ID, 20);
         storeroom.load(productStock);
+    }
+
+    @Test public void
+    fill_with_new_amount_of_given_product() {
 
         storeroom.fill(SOME_PRODUCT_ID, 5);
 
@@ -28,71 +35,23 @@ public class StoreroomShould {
 
     @Test public void
     fill_throw_exception_when_product_not_in_storeroom() {
-        Storeroom storeroom = new Storeroom(new StoreroomId(), STOREROOM_NAME);
-        ProductStock productStock = new ProductStock(SOME_PRODUCT_ID, 20);
-        storeroom.load(productStock);
         exception.expect(ProductNotFoundException.class);
 
         storeroom.fill(SOME_WRONG_PRODUCT_ID, 5);
     }
-/*
-    private static final int ZERO = 0;
-    private static final int FIVE = 5;
-    private static final int TWO = 2;
-    private static final int SEVEN = 7;
-    private Storeroom storeroom;
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+    @Test public void
+    get_stock_of_given_product() {
 
-    @Before public void
-    setUp() {
-        StoreroomId storeroomId = new StoreroomId();
-        String someName = "SomeName";
-        storeroom = new Storeroom(storeroomId, someName);
+        Stock stock = storeroom.stockOf(SOME_PRODUCT_ID);
 
+        assertThat(stock).isEqualTo(new Stock(20));
     }
 
     @Test public void
-    add_new_products_to_storeroom() {
-        Product apple = new Product("2", "Apple");
+    throw_exception_when_get_stock_from_product_not_in_storeroom() {
+        exception.expect(ProductNotFoundException.class);
 
-        storeroom.fill(apple);
-
-        assertThat(storeroom.stock(apple)).isEqualTo(ZERO);
+        storeroom.stockOf(SOME_WRONG_PRODUCT_ID);
     }
-
-    @Test public void
-    add_stock_to_storeroom() {
-        Product apple = new Product("2", "Apple");
-        storeroom.fill(apple);
-
-        storeroom.addStock(apple, FIVE);
-        storeroom.addStock(apple, TWO);
-
-        assertThat(storeroom.stock(apple)).isEqualTo(SEVEN);
-    }
-
-    @Test public void
-    consume_stock_from_storeroom() {
-        Product apple = new Product("2", "Apple");
-        storeroom.fill(apple);
-        storeroom.addStock(apple, SEVEN);
-
-        storeroom.consumeStock(apple, FIVE);
-
-        assertThat(storeroom.stock(apple)).isEqualTo(TWO);
-    }
-
-    @Test public void
-    out_of_stock_when_consume_all_products() {
-        Product apple = new Product("2", "Apple");
-        storeroom.fill(apple);
-        storeroom.addStock(apple, TWO);
-        exception.expect(OutOfStockException.class);
-
-        storeroom.consumeStock(apple, 3);
-
-    }
-*/
 }
