f0a51a31f4fdda96a27d443d6e6a122ed890686d
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-02-27 20:40:50 +0100

    Fetch storeroom by id with products (#3)

---
+++ src/main/java/cabanas/garcia/ismael/inventory/infrastructure/repository/storeroom/StoreroomRecordMapper.java
@@ -0,0 +1,15 @@
+package cabanas.garcia.ismael.inventory.infrastructure.repository.storeroom;
+
+import cabanas.garcia.ismael.inventory.domain.storeroom.model.Storeroom;
+import cabanas.garcia.ismael.inventory.domain.storeroom.model.StoreroomId;
+import cabanas.garcia.ismael.inventory.infrastructure.repository.jooq.autogen.tables.records.StoreRoomRecord;
+import org.jooq.RecordMapper;
+
+public class StoreroomRecordMapper implements RecordMapper<StoreRoomRecord, Storeroom> {
+    @Override
+    public Storeroom map(StoreRoomRecord record) {
+        return Storeroom.builder(record.getSrName())
+                .withId(new StoreroomId(record.getSrId()))
+                .build();
+    }
+}
